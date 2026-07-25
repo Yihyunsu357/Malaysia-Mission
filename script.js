5322c0027d984380f764cb7881629db00c7926c8
Yihyunsu357 <yhs52727@gmail.com>
2026-07-26 00:25:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const slides=['slides/slide1.png','slides/slide2.png','slides/slide3.png'];
-let current=0;
-const img=document.getElementById('slide');
-function show(){img.src=slides[current];}
-function nextSlide(){current=(current+1)%slides.length;show();}
-function prevSlide(){current=(current-1+slides.length)%slides.length;show();}
-setInterval(nextSlide,8000);
+const slides = [
+  'slides/slide1.png',
+  'slides/slide2.png',
+  'slides/slide3.png'
+];
+
+let current = 0;
+
+const img = document.getElementById('slide');
+
+function show() {
+  img.src = slides[current];
+}
+
+window.nextSlide = function () {
+  current = (current + 1) % slides.length;
+  show();
+};
+
+window.prevSlide = function () {
+  current = (current - 1 + slides.length) % slides.length;
+  show();
+};
+
+setInterval(window.nextSlide, 8000);
